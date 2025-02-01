877fd14e57c1eed6abb4022f6d5a09ffbaea3276
Steven A. Zaluk <stevezaluk@gmail.com>
2025-02-01 14:41:12 -0500

    Implemented two functions. This first one generates a psuedo-random number between the min and max values that you provide it, and the second one counts the number of 1 and 0 bit values in a number converted to binary

--- prime/prime.go
+++ prime/prime.go
@@ -1,3 +1,35 @@
 package prime
 
+import (
+	"math/rand/v2"
+	"strconv"
+)
 
+/*
+randomRange - Generate a pseudo-random number between the min and max provided
+*/
+func randomRange(max uint64, min uint64) uint64 {
+	return rand.Uint64N(max-min) + min
+}
+
+/*
+countBitValues - Count's the number of 1 and 0 bits in an unsigned 64-bit integer.
+*/
+func countBitValues(number uint64) (uint64, uint64) {
+	oneBit := 0
+	zeroBit := 0
+
+	binary := strconv.FormatInt(int64(number), 2)
+
+	for i := 0; i < len(binary); i++ {
+		if string(binary[i]) == "1" {
+			oneBit++
+		}
+
+		if string(binary[i]) == "0" {
+			zeroBit++
+		}
+	}
+
+	return uint64(oneBit), uint64(zeroBit)
+}
