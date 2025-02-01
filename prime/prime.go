0655c321341439fb9a9f9c49d518563889430cfc
Steven A. Zaluk <stevezaluk@gmail.com>
2025-02-01 17:50:46 -0500

    Implemented support for Generating FNV prime numbers

--- prime/prime.go
+++ prime/prime.go
@@ -1,25 +1,19 @@
 package prime
 
 import (
-	"math/rand/v2"
+	"fmt"
+	"math"
 	"strconv"
 )
 
 const (
-	primeBit32   uint16 = 32
-	primeBit64   uint16 = 64
-	primeBit128  uint16 = 128
-	primeBit256  uint16 = 256
-	primeBit512  uint16 = 512
-	primeBit1024 uint16 = 1024
+	PrimeBit32   uint16 = 32
+	PrimeBit64   uint16 = 64
+	PrimeBit128  uint16 = 128
+	PrimeBit256  uint16 = 256
+	PrimeBit512  uint16 = 512
+	PrimeBit1024 uint16 = 1024
 )
-
-/*
-randomRange - Generate a pseudo-random number between the min and max provided
-*/
-func randomRange(max uint64, min uint64) uint64 {
-	return rand.Uint64N(max-min) + min
-}
 
 /*
 countBitValues - Count's the number of 1-bits in an 64-bit unsigned integer
@@ -37,4 +31,39 @@ func countBitValues(number uint64) uint64 {
 	}
 
 	return uint64(oneBit)
+}
+
+/*
+isValid - Return's true if the prime number is valid, and false if it is not
+*/
+func isValid(prime uint64) bool {
+	left := prime % (uint64(math.Pow(2, 40) - math.Pow(2, 24) - 1))
+	right := uint64(math.Pow(2, 24) + math.Pow(2, 8) + 7)
+	if left > right {
+		return true
+	}
+
+	return false
+}
+
+/*
+GenerateFNVPrime - Generate the first FNV prime number that meets the constraints outlined
+in this article: https://datatracker.ietf.org/doc/html/draft-eastlake-fnv-17.html#section-2.1
+*/
+func GenerateFNVPrime(bitSize uint16) uint64 {
+	t := math.Floor(float64((5 + bitSize) / 12))
+
+	var prime float64
+	for b := 1; b < 255; b++ {
+		oneBits := countBitValues(uint64(b))
+		if oneBits == 4 || oneBits == 5 {
+			prime = math.Pow(256.00, t) + math.Pow(2.00, 8.00) + float64(b)
+			fmt.Println(prime, b)
+			if isValid(uint64(prime)) {
+				return uint64(prime)
+			}
+		}
+	}
+
+	return uint64(0)
 }
